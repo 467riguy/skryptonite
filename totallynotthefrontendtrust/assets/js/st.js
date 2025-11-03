51af7616037ab6af2f591bd7340b4bbc539b0f1a
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-11-03 19:48:31 +0000

    p3iowjes

--- totallynotthefrontendtrust/assets/js/st.js
+++ totallynotthefrontendtrust/assets/js/st.js
@@ -1,24 +1,18 @@
 // startup.js -- hop off, skids
-
-// Function to show the custom alert
 function showCustomAlert(message) {
   const dialog = document.getElementById('custom-alert');
   const messageEl = document.getElementById('alert-message');
 
   messageEl.textContent = message;
-  dialog.showModal(); // This automatically creates the backdrop and opens the dialog
+  dialog.showModal();
 }
-
-// Function to hide the custom alert and clear the message
 function hideCustomAlert() {
   const dialog = document.getElementById('custom-alert');
   const messageEl = document.getElementById('alert-message');
 
-  dialog.close(); // Close the dialog
-  messageEl.textContent = ''; // Clear the message content
+  dialog.close();
+  messageEl.textContent = '';
 }
-
-// Call the function to show the custom alert after a delay
 setTimeout(() => {
-  showCustomAlert("Welcome");
+  showCustomAlert("We tricked the teachers into thinking this site was educational, but it's actually a proxy.");
 }, 1000);
